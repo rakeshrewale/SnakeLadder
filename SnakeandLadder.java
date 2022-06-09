d3b32619fbe8bb63841399e0d259ccb7f7892102
rakeshrewale <rakeshrewale993@gmail.com>
2022-06-09 12:57:16 +0530

    Added: Welcome to Snake and ladder game

---
+++ SnakeandLadder.java
@@ -0,0 +1,12 @@
+package comSnakeLadder;
+
+public class SnakeandLadder {
+
+		public static void main(String[] args) {
+			
+			
+			System.out.println(" Welcome to Snake and Ladder Game");
+			
+		}
+
+	}
